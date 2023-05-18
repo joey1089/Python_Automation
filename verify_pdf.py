f72e872e04e6fc700d102bbd8194ec710acf7294
Joseph Peter <90427049+joey1089@users.noreply.github.com>
2023-05-18 00:40:34 -0400

    Update code with scan js ms code
    scan pdf for malicious code, checks if JavaScript is found.

--- verify_pdf.py
+++ verify_pdf.py
@@ -47,6 +47,64 @@ def verify_pdf(file):
         return True
     except PyPDF2.utils.PdfReadError:
         return "Corrupted"
+    
+import pdfid
+import yara
+
+def detect_malicious_code(pdf_file):
+    # Analyze the PDF structure using pdfid
+    pdf_info = pdfid.PDFiD(open(pdf_file, 'rb'))
+    pdf_info.parse()
+
+    if pdf_info.js and pdf_info.js_objects:
+        print("Potential malicious JavaScript code found.")
+
+    # Define YARA rules to detect known malicious code signatures
+    rules = """
+    rule MaliciousCode {
+        strings:
+            $malicious_code = "your malicious code signature here"
+        condition:
+            $malicious_code
+    }
+    """
+
+    # Create a YARA compiler and load the rules
+    compiler = yara.compile(source=rules)
+
+    # Scan the PDF file using the YARA rules
+    matches = compiler.match(pdf_file)
+    
+    if matches:
+        print("Malicious code signatures found:")
+        for match in matches:
+            print(f" - Rule: {match.rule}, Offset: {match.offset}")
+
+    if not pdf_info.js and not matches:
+        print("No malicious code detected.")
+
+#---- copy to separte file
+# formate to do YARA rules
+# rule MaliciousCodeSignature {
+#     strings:
+#         $pattern = "your malicious code pattern here"
+#     condition:
+#         $pattern
+# }
+# sample rule
+# rule MaliciousPDF {
+#     meta:
+#         description = "Detects potential malicious code in PDF files"
+#     strings:
+#         $eval = /[\s\(]+eval\s*\(/ nocase
+#         $base64_decode = /base64_decode\s*\(/ nocase
+#         $shellcode = { 33 C0 50 68 }
+#     condition:
+#         any of ($eval, $base64_decode) or $shellcode
+# }
+
+#---------------------------------
+# ------------------  pip install pdfid yara-python
 
 scrn_clr()
 print("============================= PDF file complete or not! ===============================")
