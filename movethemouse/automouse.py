6cf2e7d3908310e3c0a13c20d67ccfc85da51ac3
Joseph Peter <josephvpeter7@gmail.com>
2023-06-29 10:31:58 -0400

    added folder and file for automation

---
+++ movethemouse/automouse.py
@@ -0,0 +1,23 @@
+#reference - https://www.youtube.com/watch?v=ms42RwgAGKQ\
+#Use case for automation of desktop apps
+import pyautogui as pag
+import random
+import time
+
+curr_coords = pag.position() #current position of the mouse
+afk_counter = 0 #away from the keys
+while True:
+    if pag.position() == curr_coords:
+        afk_counter += 1
+    else:
+        afk_counter = 0
+        curr_coords = pag.position()
+    if afk_counter > 5:
+        x = random.randint(450,750)
+        y = random.randint(1,800)
+        pag.moveTo()
+        curr_coords = pag.position()
+    print(f"AFK Counter: {afk_counter}")
+    time.sleep()
+
+
