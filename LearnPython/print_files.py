f7c4c39b6313bd1e661251a2b99f08c73c26a2e9
joey1089 <josephvpeter7@gmail.com>
2024-04-04 15:14:57 -0400

    updated UI improvements

--- LearnPython/print_files.py
+++ LearnPython/print_files.py
@@ -1,3 +1,5 @@
+# Quick way to find all the files in your current directory, ignores the git related files expect .gitignore
+
 import re
 import os
 
@@ -8,25 +10,32 @@ def list_out_files(directory='.'):
     for root, dirs, files in os.walk(directory):
         for file_name in files:
             file_path = os.path.join(root, file_name)
-            print(f"Current File path : {file_path}")
+            #print(f"Current File path : {file_path}") # to be removed
             pattern = r".\/.git/" # regular expression to filter out .git related files
             matches = re.findall(pattern, file_path)
-            print(matches)
+            #print(matches) # to be removed
             if matches :
-                print("Igoring Git Files")
+               msg = "Igoring Git Files"
             else :
                 file_size = os.path.getsize(file_path)
                 file_info = {
                     'file_name': file_name,
                     'file_path': file_path,
-                    'file_size': file_size
+                    'file_size': str(file_size)
                 }
                 file_list.append(file_info)
                 
     return file_list
 
 file_list = list_out_files()
-os.system('cls' if os.name == 'nt' else 'clear')
+# os.system('cls' if os.name == 'nt' else 'clear')
 print("List of Files found in the current directories!")
 for file_info in file_list:
-    print(file_info)
+    # Create an empty string
+    str = ""    
+    # Convert the dictionary keys into a string
+    # using for loop only
+    for item in file_info:
+        str += item + ": " + file_info[item] + " | "
+        # print(type(str))
+    print(str)
