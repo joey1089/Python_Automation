4ab6449a3bda8a6a0564397f151132dc3b15b340
joey1089 <josephvpeter7@gmail.com>
2024-04-04 14:51:04 -0400

    updated codes

--- LearnPython/print_files.py
+++ LearnPython/print_files.py
@@ -2,16 +2,19 @@ import re
 import os
 
 
-def generate_file_list(directory='.'):
+def list_out_files(directory='.'):
     file_list = []
     matches = ""
     for root, dirs, files in os.walk(directory):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             print(f"Current File path : {file_path}")
-            pattern = r"\.git\/"
-            
-            if matches != re.findall(pattern, file_path):
+            pattern = r".\/.git/" # regular expression to filter out .git related files
+            matches = re.findall(pattern, file_path)
+            print(matches)
+            if matches :
+                print("Igoring Git Files")
+            else :
                 file_size = os.path.getsize(file_path)
                 file_info = {
                     'file_name': file_name,
@@ -19,8 +22,11 @@ def generate_file_list(directory='.'):
                     'file_size': file_size
                 }
                 file_list.append(file_info)
+                
     return file_list
 
-file_list = generate_file_list()
+file_list = list_out_files()
+os.system('cls' if os.name == 'nt' else 'clear')
+print("List of Files found in the current directories!")
 for file_info in file_list:
     print(file_info)
