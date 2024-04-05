c810c66a103ea72f9b9b3c9387424247b08c6eb7
joey1089 <josephvpeter7@gmail.com>
2024-04-04 21:25:57 -0400

    updated code

--- LearnPython/print_files.py
+++ LearnPython/print_files.py
@@ -19,9 +19,9 @@ def list_out_files(directory='.'):
             else :
                 file_size = os.path.getsize(file_path)
                 file_info = {
-                    'File_name': file_name,
-                    'Path': file_path,
-                    'File_size': str(file_size)
+                    'File_Name': file_name,
+                    'File_Path': file_path,
+                    'File_Size': str(file_size)
                 }
                 file_list.append(file_info)
                 
@@ -30,14 +30,17 @@ def list_out_files(directory='.'):
 file_list = list_out_files()
 os.system('cls' if os.name == 'nt' else 'clear')
 print("List of Files found in the current directories!\n")
-# for files in file_list:
-#     file_info = "" #create a empty string to later store dict
-#     for item,value in files.items():
-#         file_info += item + ": " + value + " | "
-#     print(file_info)
-# print("\n =========================================================== \n")
-cols = [ "File_name", "File_Path", "File_size"]
+for files in file_list:
+    file_info = "" #create a empty string to later store dict
+    for item,value in files.items():
+        file_info += item + ": " + value + " | "
+    print(file_info)
+print("\n =========================================================== \n")
+# cols = [ "File_name", "File_Path", "File_size"]
 pd = pandas.DataFrame.from_dict(file_list)
 # pd = pandas.DataFrame.from_dict(file_list, orient='index', columns='cols') 
-#https://builtin.com/data-science/dictionary-to-dataframe
+# https://builtin.com/data-science/dictionary-to-dataframe
+print(pd)
+print("\n ========================================== \n ")
+pd.set_index('File_Name', inplace=True)
 print(pd)
